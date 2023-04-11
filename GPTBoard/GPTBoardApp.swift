fede7dd6017950c00f736f0a904a35f7fd3a1c40
karankhurana <karankhurana15234@gmail.com>
2023-04-10 23:26:08 -0700

    Initial Commit

---
+++ GPTBoard/GPTBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTBoardApp.swift
+//  GPTBoard
+//
+//  Created by Karan Khurana on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GPTBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
